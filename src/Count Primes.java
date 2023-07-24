0331498f6d0200aca5becca03337f56771e0aad2
RoshanEraniyan <e.roshan141@gmail.com>
2023-07-24 04:26:36 -1200

    Initial commit Done

--- src/Count Primes.java
+++ src/Count Primes.java
@@ -1,16 +1,15 @@
-
 import java.util.*;
 
 
 class CountPrimes
 {
-    public static void countprimes(int n)
+    public static void count(int n)
     {
         int prime_count=0;
         for(int i=1;i<n;i++)
         {
             int count=0;
-            for(int j=1;j<=i;j++)
+            for(int j=1;j<n;j++)
             {
                 if(i%j==0)
                 {
@@ -23,15 +22,16 @@ class CountPrimes
             }
             if(count==2)
             {
-              prime_count++;
+                prime_count++;
             }
         }
         System.out.println(prime_count);
     }
-    public static void main(String[] args)
+    public static void main(String args[])
     {
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
-        countprimes(n);
+        int[] array=new int[n];
+        count(n);
     }
 }
